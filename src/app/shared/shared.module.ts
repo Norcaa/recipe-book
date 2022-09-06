52cfed99b626b133b45bbf422d4eac6d2adec625
Kovács Nikoletta <Niki0505@Citromail.hu>
2022-09-06 12:57:09 +0200

    Add modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout'; // Take care of the import, it's not module!!
+import { MaterialModule } from 'material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 const MODULES = [
+    MaterialModule,
+    FlexLayoutModule,
+    HttpClientModule
 ]
 
 @NgModule({
