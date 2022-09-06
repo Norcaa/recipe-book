3f7608916f87f168e3f9e54a11f6642f402031c8
Kovács Nikoletta <Niki0505@Citromail.hu>
2022-09-06 12:58:58 +0200

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { NavigationService } from './core/services/navigation.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'coding-world-tutorial';
+    title = 'coding-world-tutorial'
+    selectedScreen!: string; 
+
+    constructor(private navigationService: NavigationService) {}
+
+    ngDoCheck() {
+        this.selectedScreen = this.navigationService.getScreen()
+    }
 }
