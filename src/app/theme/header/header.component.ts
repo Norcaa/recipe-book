d82c701d22ccad349ba9161d120c878c87d639a9
Kovács Nikoletta <Niki0505@Citromail.hu>
2022-09-06 12:56:08 +0200

    Set toolbar in header

--- src/app/theme/header/header.component.ts
+++ src/app/theme/header/header.component.ts
@@ -1,21 +1,26 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NavigationService } from 'src/app/core/services/navigation.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() toggleSidenav = new EventEmitter<void>()
+    @Output() toggleSidenav = new EventEmitter<void>()
 
-  constructor() { }
+    constructor(private navigationService: NavigationService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  showSidenav() {
-      this.toggleSidenav.emit()
-  }
+    showSidenav() {
+        this.toggleSidenav.emit()
+    }
+
+    onSelect(screenName: string) {
+        this.navigationService.setScreen(screenName)
+    }
 
 }
