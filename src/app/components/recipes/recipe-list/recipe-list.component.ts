42233ff7a5fff32ccdf86ebeff4ae484d861e5cb
Kovács Nikoletta <Niki0505@Citromail.hu>
2022-09-06 12:59:56 +0200

    Update recipe list

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Ingredient } from 'src/app/core/models/ingredient';
+import { Recipe } from 'src/app/core/models/recipe';
+import { RecipeService } from 'src/app/core/services/recipe.service';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+    @Output() selectedRecipe = new EventEmitter<Recipe>()
 
-  ngOnInit(): void {
-  }
+    recipes: Recipe[] = []
+    //route: string = ''
 
+    constructor(private recipeService: RecipeService, private router: ActivatedRoute) { }
+
+    ngOnInit(): void {
+        this.recipeService.getRecipes().subscribe(recipes => {
+            this.recipes = recipes
+        })
+
+        /* this.router.url.subscribe(url => {
+            console.log(url)
+        }) */
+    }
+
+    selectRecipe(selectedRecipe: Recipe) {
+        this.selectedRecipe.emit(selectedRecipe)
+    }
 }
