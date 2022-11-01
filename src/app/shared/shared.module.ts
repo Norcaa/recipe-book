079f3d8a71471f7f183cc261bd9d5e52ea7b8d49
Kovács Nikoletta <Niki0505@Citromail.hu>
2022-11-01 22:45:38 +0100

    Update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,21 +3,37 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout'; // Take care of the import, it's not module!!
 import { MaterialModule } from 'material.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ShareButtonComponent } from './share-button/share-button.component';
+import { ShareModule } from 'ngx-sharebuttons';
+import { PreparationStepComponent } from './preparation-step/preparation-step.component'
+import { ReactiveFormsModule } from '@angular/forms';
+import { NotifierModule } from 'angular-notifier';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { SubmitButtonComponent } from './submit-button/submit-button.component';
+
 
 const MODULES = [
+    CommonModule,
     MaterialModule,
     FlexLayoutModule,
-    HttpClientModule
+    HttpClientModule,
+    ShareModule,
+    ReactiveFormsModule,
+    NotifierModule
 ]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ...MODULES
+  declarations: [
+    ShareButtonComponent,
+    PreparationStepComponent,
+    SubmitButtonComponent
   ],
+  imports: [ ...MODULES, AngularSvgIconModule.forRoot() ],
   exports: [
-    ...MODULES
+    ...MODULES,
+    ShareButtonComponent,
+    SubmitButtonComponent,
+    AngularSvgIconModule
   ]
 })
 export class SharedModule { }
