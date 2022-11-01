3785c2cbdce5cacef8d4868e66395755f0ec876f
Kovács Nikoletta <Niki0505@Citromail.hu>
2022-11-01 22:38:54 +0100

    Update recipes module

--- src/app/components/recipes/recipes.module.ts
+++ src/app/components/recipes/recipes.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipesComponent } from './recipes.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component'; 
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RecipeFullComponent } from './recipe-full/recipe-full.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CategoryItemComponent } from './categories/category-item/category-item.component';
+import { NewRecipeComponent } from './new-recipe/new-recipe.component';
+import { CategoryPickerComponent } from './new-recipe/category-picker/category-picker.component';
+import { RecipeListItemComponent } from './recipe-list/recipe-list-item/recipe-list-item.component';
+import { RecipeGridItemComponent } from './recipe-list/recipe-grid-item/recipe-grid-item.component';
+import { RecipeStreamItemComponent } from './recipe-list/recipe-stream-item/recipe-stream-item.component';
 
 const MODULES = [
     RecipesComponent,
-    RecipeDetailComponent,
     RecipeListComponent,
-    RecipeItemComponent,
-    RecipeFullComponent
+    RecipeListItemComponent,
+    RecipeFullComponent,
+    RecipeGridItemComponent,
+    RecipeStreamItemComponent,
+    CategoriesComponent,
+    CategoryItemComponent,
+    CategoryPickerComponent,
+    NewRecipeComponent
 ]
 
 @NgModule({
@@ -24,7 +34,7 @@ const MODULES = [
     ],
     imports: [
         CommonModule,
-        SharedModule 
+        SharedModule
     ]
 })
 export class RecipesModule { }
