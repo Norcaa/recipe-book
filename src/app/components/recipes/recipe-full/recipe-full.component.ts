fc90677807ae4e16404c12946d3ecb321f94b527
Kovács Nikoletta <Niki0505@Citromail.hu>
2022-11-01 22:32:44 +0100

    Fix full recipe page

--- src/app/components/recipes/recipe-full/recipe-full.component.ts
+++ src/app/components/recipes/recipe-full/recipe-full.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Category } from 'src/app/core/models/category';
 import { Recipe } from 'src/app/core/models/recipe';
+import { CategoryService } from 'src/app/core/services/category.service';
 import { RecipeService } from 'src/app/core/services/recipe.service';
 
 @Component({
@@ -10,10 +12,14 @@ import { RecipeService } from 'src/app/core/services/recipe.service';
 })
 export class RecipeFullComponent implements OnInit {
 
-    recipename: string = 'Big Night Pizza'
+    recipename: string = 'big-night-pizza'
     recipe!: Recipe; 
+    category!: Category
     
-    constructor(private router: ActivatedRoute, private recipeService: RecipeService) { }
+    constructor(
+        private router: ActivatedRoute, 
+        private recipeService: RecipeService, 
+        private categoryService: CategoryService) { }
 
     ngOnInit(): void {
         this.router.params.subscribe(param => {
@@ -23,15 +29,26 @@ export class RecipeFullComponent implements OnInit {
             this.recipeService.getRecipes().subscribe(recipes => {
                 this.findRecipe(recipes)
             })
-        })        
+        })
     }
 
     findRecipe(recipes: Recipe[]) {
         recipes.forEach(recipe => {
             if (this.recipename == recipe.slug) {
                 this.recipe = recipe
+                this.findCategory()
             }
         })
     }
 
+    findCategory() {
+        this.category = new Category(1, 'Meat', 'local_pizza', 'https://staticfanpage.akamaized.net/wp-content/uploads/sites/22/2020/06/iStock-1141704314-1200x675.jpg')
+        /* this.categoryService.getCategories().subscribe(datas => {
+            datas.forEach(category => {
+                if (this.recipe.category = category.id) {
+                    this.category = category
+                }
+            })
+        }) */
+    }
 }
