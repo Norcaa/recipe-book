293f063c816905f6cf735903c4c2ae7f6f466f2a
Kovács Nikoletta <Niki0505@Citromail.hu>
2022-11-01 22:26:39 +0100

    Add authentication routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { ProfileComponent } from './components/auth/profile/profile.component';
 import { HomeComponent } from './components/home/home.component';
+import { CategoriesComponent } from './components/recipes/categories/categories.component';
+import { NewRecipeComponent } from './components/recipes/new-recipe/new-recipe.component';
 import { RecipeFullComponent } from './components/recipes/recipe-full/recipe-full.component';
 import { RecipeListComponent } from './components/recipes/recipe-list/recipe-list.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
@@ -11,6 +16,27 @@ const routes: Routes = [
         path: '',
         /*  component: HomeComponent, */
         children: [
+            {
+                path: 'auth',
+                children: [
+                    {
+                        path: 'login',
+                        component: AuthComponent
+                    },
+                    {
+                        path: 'registration',
+                        component: AuthComponent
+                    },
+                    {
+                        path: 'profile',
+                        component: ProfileComponent
+                    },
+                    {
+                        path: 'forgot-password',
+                        component: ForgotPasswordComponent
+                    }
+                ]
+            },
             {
                 path: 'recipe',
                 children: [
